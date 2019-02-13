ff66cfae01b38ca64a52127735c5e038d03647dc
Víctor Bernabé Pérez <victorbernabeperez@gmail.com>
2019-02-13 18:50:19 +0100

    [feat] Add basic functionality

---
+++ background.js
@@ -0,0 +1,17 @@
+// Create the context menu
+chrome.runtime.onInstalled.addListener(function() {
+  for (let key of Object.keys(websites)) {
+    chrome.contextMenus.create({
+      id: key,
+      title: websites[key],
+      type: 'normal',
+      contexts: ['selection'],
+    });
+  }
+});
+
+// Search Kanji on the website on click
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+  let url = item.menuItemId + item.selectionText;
+  chrome.tabs.create({url: url, index: tab.index + 1});
+});
